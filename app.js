9d2491f86a9323d8ac5fd8886bbf89f42aeb8cdf
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-06-14 17:01:16 +0530

    create campgrounds

--- app.js
+++ app.js
@@ -22,22 +22,37 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
+app.use(express.urlencoded({ extended: true }));    //to handle post requests as our res.body will be empty otherwise,here we parse it
+
+
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
 
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
-})
+});
+
+
+//order matters,this should be before the ':id' one
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground); //our body gets a campground object as the name specified in the get form was campground[title] kinda
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
 
 
 app.get('/campgrounds/:id', async (req, res,) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
 });
-
 
 
 app.listen(3000, () => {
