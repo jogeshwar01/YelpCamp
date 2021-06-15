cc21280e79dfd7a8a249b679d5357e3dc27f3458
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-06-15 12:09:44 +0530

    add images

--- seeds/index.js
+++ seeds/index.js
@@ -25,9 +25,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});    //empty database before adding new values
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid sunt, error voluptate, natus deleniti obcaecati qui earum consequuntur, pariatur ratione quo suscipit! Veniam voluptas provident corporis aliquam illum velit quis.',
+            price: price
         })      //title randomly taken using sample function
         await camp.save();
     }
