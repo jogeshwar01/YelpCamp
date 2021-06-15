8eed1a4c649bd0f2d25888c357ba9102cb39d749
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-06-15 17:52:37 +0530

    add catchAsync wrapper function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
 
 const Campground = require("./models/campground");
 
@@ -35,10 +36,10 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
-});
+}))
 
 
 //order matters,this should be before the ':id' one
@@ -47,39 +48,42 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground); //our body gets a campground object as the name specified in the get form was campground[title] kinda
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-});
+}))
 
 
-app.get('/campgrounds/:id', async (req, res,) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res,) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
-});
+}))
 
 
 //edit request -- we use PUT here in edit.ejs by faking it using method-override
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground });
-})
+}))
 
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
-});
+}))
 
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-})
+}))
 
+app.use((err, req, res, next) => {
+    res.send('Oh boy, something went wrong!')
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
