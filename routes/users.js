d645e7bd487ba94640a791a649fb86b8c4aaf522
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-06-21 16:39:01 +0530

    login post route

--- routes/users.js
+++ routes/users.js
@@ -32,6 +32,10 @@ router.get('/login', (req, res) => {
 })
 
 
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'welcome back!');
+    res.redirect('/campgrounds');
+})
 
 
 module.exports = router;
