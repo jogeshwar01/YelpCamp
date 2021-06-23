7dc63013235372a3c88d3acfa6ae479a476942eb
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-06-23 11:56:54 +0530

    refactor users controller

--- routes/users.js
+++ routes/users.js
@@ -4,47 +4,21 @@ const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
-
-router.get('/register', (req, res) => {
-    res.render('users/register');
-});
+const users = require('../controllers/users');
 
 
-router.post('/register', catchAsync(async (req, res, next) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash('success', 'Welcome to Yelp Camp!');
-            res.redirect('/campgrounds');
-        })
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('register');
-    }
-}));
+router.get('/register', users.renderRegister);
 
 
-router.get('/login', (req, res) => {
-    res.render('users/login');
-})
+router.post('/register', catchAsync(users.register));
 
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', 'welcome back!');
-    //to keep track of where user was intitially requesting when they were not logged in
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;    //to delete the url from our session after it is taken into calculation
-    res.redirect(redirectUrl);
-})
+router.get('/login', users.renderLogin)
 
-router.get('/logout', (req, res) => {
-    req.logout();
-    req.flash('success', "Goodbye!");
-    res.redirect('/campgrounds');
-})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
+
+router.get('/logout', users.logout)
 
 module.exports = router;
 
