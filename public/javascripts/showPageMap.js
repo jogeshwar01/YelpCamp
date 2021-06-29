4a61fa40cddca9c0270bf0a6c57a049a58a7af31
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-06-29 12:27:14 +0530

    display general map

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = mapToken;   //ejs won't work here directly so we added additional js in show.ejs
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
