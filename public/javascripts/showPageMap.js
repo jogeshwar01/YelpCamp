e2bf4ac6dca92278b16cb4997593ca664ba5b8ed
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-06-29 13:24:41 +0530

    map popup

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,11 +1,17 @@
 mapboxgl.accessToken = mapToken;   //ejs won't work here directly so we added additional js in show.ejs
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL --default set -can be changed
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
 
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
+            )
+    )
     .addTo(map)
